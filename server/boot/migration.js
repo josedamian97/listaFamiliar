8336874300c06b2db60535f0ef11aca4397e2a96
Jose Damian <josedamian03@gmail.com>
2017-11-20 10:41:12 +0100

    configurando datasources.local

---
+++ server/boot/migration.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var config = require('../config.local.js');
+
+module.exports = function(app) {
+    //data sources
+    var mysqlDs = app.dataSources.aplicacionMYSQL;
+
+    
+    if(process.env.AUTOMIGRATE) {
+        mysqlDs.automigrate(null, function (err) {
+            if (err) console.log(err);
+            console.log('> Models migrated to tables');
+            
+        });
+    }
+};
