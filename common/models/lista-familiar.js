67055dcd27eb513435eb74a639d9db501be7bcbb
Jose Damian <josedamian03@gmail.com>
2017-11-21 11:37:06 +0100

    propietario owner

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Listafamiliar) {
-
+    Listafamiliar.beforeRemote('create', function (context, Listafamiliar, next){
+        context.args.data.owner = context.req.accessToken.userId;
+        next();
+    });
 };
