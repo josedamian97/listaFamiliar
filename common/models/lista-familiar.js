aa5eb2101f43b5d2c000637a0fbbfb06ae9e4e9d
Jose Damian <josedamian03@gmail.com>
2017-11-22 12:27:53 +0100

    listaOwner terminada

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -5,4 +5,14 @@ module.exports = function(Listafamiliar) {
         context.args.data.owner = context.req.accessToken.userId;
         next();
     });
+    
+    Listafamiliar.afterRemote('create', function (context, listafamiliar, next){
+        var Usuario = Listafamiliar.app.models.Usuario; //cogemos la tabla de usuario
+        Usuario.findById(context.req.accessToken.userId, function(err,objetousuario){
+            objetousuario.listaFamiliarId=listafamiliar.id;
+            objetousuario.save(function (err){
+                next();
+            });        
+        });                  
+    });
 };
