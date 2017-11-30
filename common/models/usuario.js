403cf0936f76c7554325a4a23d57a883cb12fcc2
Jose Damian <josedamian03@gmail.com>
2017-11-30 11:38:53 +0100

    aceptar solicitud sin terminar

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -2,4 +2,29 @@
 
 module.exports = function(Usuario) {
 
+/**
+ * le enviamos el id del usuario y si tiene alguna solicitud en la lista de miembros, se le aprobara la solicitud
+ * @param {object} contexto el morcillon
+ * @param {Function(Error, array)} callback
+ */
+
+Usuario.prototype.aceptarSolicitud = function(contexto, callback) {
+  var array;
+  var usuarioId = contexto.req.accessToken.userId;
+  var usuario=this;
+  var listaId;
+  
+  Usuario.findById(usuarioId,
+  function(err, objetousuario) {
+      listaId=objetousuario.listaFamiliarId;
+  });
+
+  Usuario.hasnadbelongstomany({where:{"",
+  function(err, usuariometodo) {
+  
+});
+  //callback(null, array);
+};
+
+
 };
