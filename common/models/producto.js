533d590bbd61dc4090c2ba8e52aaf8bb99df1a85
Jose Damian <josedamian03@gmail.com>
2017-12-04 09:16:24 +0100

    limpiarLista terminado

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Producto) {
+/**
+ * poner en "false" el atributo comprar de todos los productos de la lista familiar del usuario logeado
+ * @param {object} contexto el morcillon
+ * @param {Function(Error, array)} callback
+ */
+
+Producto.limpiarLista = function(contexto, callback) {
+  var usuarioId = contexto.req.accessToken.userId;
+  var Uzuario = Producto.app.models.Usuario;
+  var array;
+  var listaUzuario;
+  Uzuario.findById(usuarioId, function(err,objetoUzuario){
+      listaUzuario = objetoUzuario.listaFamiliarId;
+      Producto.updateAll({listaFamiliarId: listaUzuario}, {comprar:0}, function(err, info) {
+      Producto.find({where:{"listaFamiliarId":listaUzuario}}, function(err, array){
+          callback(null, array);
+      });
+      });
+  });
+};
 
 };
