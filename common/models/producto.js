5e97d849458e091e8b9294981fdcab4e8c2fba83
Jose Damian <josedamian03@gmail.com>
2017-12-05 11:53:09 +0100

    productoCompradoViceversa

--- common/models/producto.js
+++ common/models/producto.js
@@ -22,4 +22,50 @@ Producto.limpiarLista = function(contexto, callback) {
   });
 };
 
+/**
+ * si es true lo ponemos false y viceversa de un id de producto
+ * @param {object} contexto el morcillon
+ * @param {Function(Error, array)} callback
+ */
+
+Producto.prototype.comprado = function(contexto, callback) {
+  var array;
+  var usuarioId = contexto.req.accessToken.userId;
+  var idProducto = this.id;
+  var estado = this.comprar;
+  var listaId= this.listafamiliarId;
+  var uzuario = Producto.app.models.Usuario;
+  var listaUzuario;
+  
+  uzuario.findById(usuarioId, function (err,objetousuario){
+     listaUzuario=objetousuario.listaFamiliarId;
+     
+     if(listaId == listaUzuario){
+         if(estado == 0){
+            Producto.updateAll({id: idProducto}, {comprar: 1}, function(err, info) {
+                Producto.find({where:{"listafamiliarId":listaUzuario}}, function(err, array){
+          callback(null, array);
+      });
+        });
+        
+    
+        }else if(estado == 1){
+            Producto.updateAll({id: idProducto}, {comprar: 0}, function(err, info) {
+                Producto.find({where:{"listafamiliarId":listaUzuario}}, function(err, array){
+          callback(null, array);
+      });
+        });
+        
+        }
+     }else{
+         callback("Ese producto no pertenece a la lista del usuario autenticado");
+     }
+  });
+  
+  
+  
+  
+};
+
+
 };
